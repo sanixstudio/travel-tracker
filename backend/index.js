8e68a01720c78f63d0cdf7996daf0907edf1be7d
Adnan Niaz <sanixstudio@gmail.com>
2023-04-29 13:52:01 -0700

    feat: added dotenv for DB connect

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
 const cors = require("cors");
 const app = express();
+dotenv.config();
 
+app.use(morgan("common"));
 app.use(express.json());
 app.use(cors());
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Database"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello from backend");
+});
+
 const PORT = process.env.PORT || 4001;
-app.listen(PORT, () => "Listening on http://localhost:" + PORT);
+app.listen(PORT, () => `Listening on http://localhost: ${PORT}`);
