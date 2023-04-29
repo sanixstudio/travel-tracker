158ac20e58f4f3a7b3fe4a8c80a8837c295aab0a
Adnan Niaz <sanixstudio@gmail.com>
2023-04-29 14:31:24 -0700

    feat: created schemas

---
+++ backend/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+      min: [3, "Username must be at least 3 letter"],
+      max: [20, "20 is max character limit"],
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      max: [40, "40 is max character limit"],
+    },
+    password: {
+      type: String,
+      require: true,
+      min: 3,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.Schema("User", UserSchema);
